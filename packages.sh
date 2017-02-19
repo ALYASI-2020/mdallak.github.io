d539435916170a8199248c884308ceff06df9af9
mdallak <m_dallak@outlook.com>
2017-02-19 21:13:42 +0400

    “init”

--- packages.sh
+++ packages.sh
@@ -1,4 +1,1 @@
-dpkg-deb -bZgzip projects/IconTool debs
-dpkg-deb -bZgzip projects/25pp debs
-dpkg-deb -bZgzip projects/AppSync Unified debs
-dpkg-deb -bZgzip projects/CyFree debs
+dpkg-deb -bZgzip projects/NUVO debs
