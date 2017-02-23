cffeda6cf93168ecd927b64702954c010f9a1806
mdallak <m_dallak@outlook.com>
2017-02-23 22:12:00 +0400

    “init”

--- packages.sh
+++ packages.sh
@@ -1,4 +1,4 @@
 dpkg-deb -bZgzip projects/TweakMode debs
-dpkg-deb -bZgzip projects/LoveIsComplicatedAgain debs
+dpkg-deb -bZgzip projects/com.Misti.loveiscomplicatedagain debs
 dpkg-deb -bZgzip projects/KGPayphone debs
 dpkg-deb -bZgzip projects/CyDown debs
