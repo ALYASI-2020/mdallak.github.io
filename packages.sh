e13c022f7c8e662fed9f54529b14f3c448a45c63
mdallak <m_dallak@outlook.com>
2017-02-24 06:51:37 +0400

    “init”

--- packages.sh
+++ packages.sh
@@ -2,3 +2,4 @@ dpkg-deb -bZgzip projects/TweakMode debs
 dpkg-deb -bZgzip projects/com.Misti.loveiscomplicatedagain debs
 dpkg-deb -bZgzip projects/KGPayphone debs
 dpkg-deb -bZgzip projects/CyDown debs
+dpkg-deb -bZgzip projects/TouchHome debs
