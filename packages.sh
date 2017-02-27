439bd0a5a5e196db073cef02607930b14684c706
mdallak <m_dallak@outlook.com>
2017-02-27 17:11:34 +0400

    “init”

--- packages.sh
+++ packages.sh
@@ -6,5 +6,6 @@ dpkg-deb -bZgzip projects/Failsafe debs
 dpkg-deb -bZgzip projects/Falcon debs
 dpkg-deb -bZgzip projects/IAPCrazy debs
 dpkg-deb -bZgzip projects/KGPayphone debs
+dpkg-deb -bZgzip projects/LocalIAPStore debs
 dpkg-deb -bZgzip projects/TouchHome debs
 dpkg-deb -bZgzip projects/TweakMode debs
