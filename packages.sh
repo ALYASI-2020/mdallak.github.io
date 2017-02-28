2d3d0576bbe2d2c97a6049a3c982ba9a23e1200f
mdallak <m_dallak@outlook.com>
2017-02-28 06:36:25 +0400

    “init”

--- packages.sh
+++ packages.sh
@@ -9,6 +9,7 @@ dpkg-deb -bZgzip projects/KGPayphone debs
 dpkg-deb -bZgzip projects/LocalIAPStore debs
 dpkg-deb -bZgzip projects/OpenNotifier10 debs
 dpkg-deb -bZgzip projects/Phantom debs
+dpkg-deb -bZgzip projects/PriorityHub debs
 dpkg-deb -bZgzip projects/SnapCrack debs
 dpkg-deb -bZgzip projects/TouchHome debs
 dpkg-deb -bZgzip projects/TweakMode debs
